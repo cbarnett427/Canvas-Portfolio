a63e32976e87293d6751f9043669a53bf00279d5
Clayton Barnett <Cbarnett427@live.com>
2022-09-21 12:54:42 -0500

    Update v1.1

--- js/main.js
+++ js/main.js
@@ -18,3 +18,18 @@ function closeMenu() {
 }
 
 
+window.onscroll = function() {scrollFunction()};
+ 
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      
+      document.getElementById("navbar").style.background = "#fff";
+      document.getElementById("nav-menu").style.color = "#3f3f46";
+      document.getElementById("nav-logo").style.color = "#3f3f46";
+    } else {
+    
+      document.getElementById("navbar").style.background = "none";
+      document.getElementById("nav-menu").style.color = "#fff";
+      document.getElementById("nav-logo").style.color = "#fff";
+    }
+  }
