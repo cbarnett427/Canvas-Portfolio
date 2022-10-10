3215372b7db9f978de8b3d1fa718a90b20eb94f1
Clayton Barnett <Cbarnett427@live.com>
2022-10-10 13:42:51 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,83 +5,74 @@ const lineMenu = document.querySelectorAll(".line-menu");
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  lineMenu.forEach((n) => n.style.background = "#3f3f46");
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+	lineMenu.forEach((n) => n.classList.add("active"));
 }
-
-
 const navLink = document.querySelectorAll(".nav-link");
 navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-  hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
-  wrapperMenu.classList.toggle('open');
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+	wrapperMenu.classList.remove('open');
 }
-
-
 // OPEN AND CLOSE WRAPPERMENU ON LINK CLICK
 var wrapperMenu = document.querySelector('.wrapper-menu');
-  wrapperMenu.addEventListener('click', function(){
-  wrapperMenu.classList.toggle('open'); 
-})
+wrapperMenu.addEventListener('click', function() {
+		wrapperMenu.classList.toggle('open');
+	})
 
 
-// NAVIGATION ON SCROLL
+// ---------- NAVIGATION ON SCROLL ----------
 let section = document.querySelectorAll("section");
 let menu = document.querySelectorAll("header nav a");
 var navbar = document.getElementById("navbar");
 var navlogo = document.getElementById("nav-logo");
-
 window.onscroll = () => {
-  if (window.scrollY > 20) {
-    navbar.style.backgroundColor = "#fff";
-    navbar.style.boxShadow = "1px 3px 5px rgba(0, 0, 0, 0.10)";
-    navbar.style.height = "4.375rem";
-    navbar.style.color = "#3f3f46";
-    navlogo.style.color = "#3f3f46";
-    lineMenu.forEach((n) => n.style.background = "#3f3f46");
-  } else {
-    navbar.style.backgroundColor = "transparent";
-    navbar.style.boxShadow = "none";
-    navbar.style.height = "5rem";
-    navbar.style.color = "#fff";
-    navlogo.style.color = "#fff";
-    lineMenu.forEach((n) => n.style.background = "#fff");
-  }
-  section.forEach((i) => {
-    let top = window.scrollY;
-    let offset = i.offsetTop - 250;
-    let height = i.offsetHeight;
-    let id = i.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      menu.forEach((link) => {
-        link.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]").classList.add("active");
-      });
-    }
-  });
+	if(window.scrollY > 20) {
+		navbar.style.backgroundColor = "#fff";
+		navbar.style.boxShadow = "1px 3px 5px rgba(0, 0, 0, 0.10)";
+		navbar.style.height = "4.375rem";
+		navbar.style.color = "#3f3f46";
+		navlogo.style.color = "#3f3f46";
+		lineMenu.forEach((n) => n.classList.add("active"));
+	} else {
+		navbar.style.backgroundColor = "transparent";
+		navbar.style.boxShadow = "none";
+		navbar.style.height = "5rem";
+		navbar.style.color = "#fff";
+		navlogo.style.color = "#fff";
+		lineMenu.forEach((n) => n.classList.remove("active"));
+	}
+	section.forEach((i) => {
+		let top = window.scrollY;
+		let offset = i.offsetTop - 250;
+		let height = i.offsetHeight;
+		let id = i.getAttribute("id");
+		if(top >= offset && top < offset + height) {
+			menu.forEach((link) => {
+				link.classList.remove("active");
+				document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+			});
+		}
+	});
 };
 
 
 // ---------- REVEAL ELEMENTS ON SCROLL ----------
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
+	var reveals = document.querySelectorAll(".reveal");
+	for(var i = 0; i < reveals.length; i++) {
+		var windowHeight = window.innerHeight;
+		var elementTop = reveals[i].getBoundingClientRect().top;
+		var elementVisible = 150;
+		if(elementTop < windowHeight - elementVisible) {
+			reveals[i].classList.add("active");
+		} else {
+			reveals[i].classList.remove("active");
+		}
+	}
 }
 window.addEventListener("scroll", reveal);
 // To check the scroll position on page load
